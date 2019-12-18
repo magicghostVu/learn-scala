d93376f508985e0c9ff9979acbfa2e0ab5e0ec9c
magicghostvu <magicghost.vu@gmail.com>
2019-12-18 21:49:12 +0700

    #fix file mState

--- src/main/scala/functional/use_monad/MState.scala
+++ src/main/scala/functional/use_monad/MState.scala
@@ -2,6 +2,26 @@ package functional.use_monad
 
 trait MState[S, +A] {
     def apply(s: S): (S, A)
+
+    def map[B](fmap: A => B): MState[S, B] = {
+        val newApp = (apply _).andThen(tsa => {
+            (tsa._1, fmap(tsa._2))
+        })
+        //newApp
+        new MState[S, B] {
+            override def apply(s: S): (S, B) = newApp(s)
+        }
+    }
+
+    def flatMap[B](fFlatMap: A => MState[S, B]): MState[S, B] = {
+        val newApp = (apply _).andThen(tsa => {
+            fFlatMap(tsa._2)(tsa._1)
+        })
+        new MState[S, B] {
+            override def apply(s: S): (S, B) = newApp(s)
+        }
+    }
+
 }
 
 object MState {
@@ -13,9 +33,11 @@ object MState {
 
     def init[S]: MState[S, S] = state[S, S](s => (s, s))
 
-    def modify[S](f: S => S) = {
-        // todo: impl
-        init[S].flatMap
+    def modify[S](fModified: S => S): MState[S, Unit] = {
+        val newApp: S => (S, Unit) = s => (fModified(s), ())
+        init[S].flatMap(s => {
+            state(newApp)
+        })
     }
 
 }

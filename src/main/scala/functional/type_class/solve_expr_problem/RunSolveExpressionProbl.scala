5dc41c1c4af55c9545a62cdaa5d3ad378596ad6e
Vũ Hồng Phú <>
2019-12-26 18:01:00 +0700

    #thêm các chú thích về type class trong giải quyết expression problem

--- src/main/scala/functional/type_class/solve_expr_problem/RunSolveExpressionProbl.scala
+++ src/main/scala/functional/type_class/solve_expr_problem/RunSolveExpressionProbl.scala
@@ -6,7 +6,7 @@ case class Employee(id: Int, name: String)
 case class ContractEmployee(contractId: Int, name: String)
 
 
-// A ở đây thể hiên loại nhân viên gì
+// A ở đây thể hiên loại nhân viên gì, có thể là nhân viên hợp đồng hoặc nhân viên chính thức
 case class USPayroll[TypePayee](seq: Seq[TypePayee]) {
     def processPay(implicit processor: PayrollProcessor[USPayroll, TypePayee]): Either[String, Throwable] = {
         processor.processPayroll(seq)
@@ -28,9 +28,14 @@ trait PayrollProcessor[Payroll[_], TypePayee] {
 
 //tất cả các kiểu pay được khai báo implicit ở một nơi và sẽ được import theo ngữ cảnh sau này để trình biên dịch tự
 // match với các đối tượng tương ứng
+// object này gióng như một ma trận mà ở đó ta có thể mở rộng cả về 2 phía
+// ta có thể thêm processor mới bất cứ khi nào với Payroll mới và/hoặc là kiểu payee mới đều không có vấn đề
 object AllPayrollProcessor {
 
     implicit object USPayrollProcessor extends PayrollProcessor[USPayroll, Employee] {
+
+        // todo: impl logic trả lương cho từng case bên trong này
+        // logic là hoàn toàn xác định vì quốc gia đã biết và kiểu payee đã biết
         override def processPayroll(payees: Seq[Employee]): Either[String, Throwable] = {
             Left("US pay for all")
         }

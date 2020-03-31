9140642a137bc922c2b7e605e2c9eccb7be6b068
magicghostvu <magicghost.vu@gmail.com>
2020-03-31 19:50:55 +0700

    #add method sequence, parMap

--- src/main/scala/learn_fp_in_scala/chap7/Par.scala
+++ src/main/scala/learn_fp_in_scala/chap7/Par.scala
@@ -43,16 +43,17 @@ object Par {
         }
 
 
-        // hàm này có thể bị gọi 2 lần trong 2 thread khác nhau, và dòng 50 có thể chạy 2 lần
+        // hàm này có thể bị gọi 2 lần trong 2 thread khác nhau, và dòng 51 có thể chạy 2 lần
         // tuy nhiên điều này có thể chấp nhận được
         override def get(): C = {
-            optionC match {
+            /*optionC match {
                 case None => {
                     val c: C = compute(Long.MaxValue)
                     c
                 }
                 case Some(value) => value
-            }
+            }*/
+            compute(Long.MaxValue)
         }
 
         override def get(timeout: Long, unit: TimeUnit): C = {
@@ -65,14 +66,14 @@ object Par {
         private def compute(timeoutInNanos: Long): C = {
             optionC match {
                 case None => {
-                    val start = System.nanoTime
+                    val startGetA = System.nanoTime
                     val ar = futureA.get(timeoutInNanos, TimeUnit.NANOSECONDS)
-                    val stop = System.nanoTime
-                    val aTime = stop - start
+                    val stopGetA = System.nanoTime
+                    val aTime = stopGetA - startGetA
                     val br = futureB.get(timeoutInNanos - aTime, TimeUnit.NANOSECONDS)
-                    val ret = f(ar, br)
-                    optionC = Some(ret)
-                    ret
+                    val retC = f(ar, br)
+                    optionC = Some(retC)
+                    retC
                 }
                 case Some(value) => {
                     value
@@ -96,11 +97,12 @@ object Par {
 
 
     // thực ra hàm này có vai trò như hàm combine, chứ nó không phải là map
-    def map2[A, B, C](pa: Par[A], pb: Par[B])(functionMap: (A, B) => C): Par[C] = {
+    // nó là một hình thái của flatMap, => một phép trừu tượng được giải nghĩa rất đơn giản
+    def map2[A, B, C](pa: Par[A], pb: Par[B])(functionCombine: (A, B) => C): Par[C] = {
         es: ExecutorService => {
             val futureA = pa(es)
             val futureB = pb(es)
-            UnitFuture(functionMap(futureA.get(), futureB.get()))
+            Map2Future(futureA, futureB, functionCombine)
         }
     }
 
@@ -111,9 +113,60 @@ object Par {
         es: ExecutorService => {
 
             //submit một callable cho es
-            // callable đó lại thực ra là lấy thực thi parA và lấy ra A
+            //callable đó lại thực ra là lấy thực thi parA và lấy ra A
             es.submit(() => a(es).get())
         }
+    }
+
+
+    // hàm này sẽ map song song từng phần tử rồi nối ghép các mảnh thành hoàn chỉnh 1 list
+    /*def parMap[A, B](listA: List[A])(fMap: A => B): Par[List[B]] = {
+        val listParB: List[Par[B]] = listA.map(asyncF(fMap))
+
+
+        null
+    }*/
+
+
+    // sử dụng fold, tuy nhiên cách này sẽ không chạy song song được
+    private def simpleSequence[A](ps: List[Par[A]]): Par[List[A]] = {
+        ps.foldRight[Par[List[A]]](unit(List[A]()))((pa, parListA) => {
+            map2(pa, parListA)(_ :: _)
+        })
+    }
+
+
+    private def balanceSequence[A](ps: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] = {
+        if (ps.isEmpty) {
+            unit(Vector[A]())
+        }
+        else if (ps.size == 1) {
+            val pa = ps.head
+            map(pa)(Vector(_))
+        }
+        else {
+            val (l, r) = ps.splitAt(ps.size / 2)
+            val pl = balanceSequence(l)
+            val pr = balanceSequence(r)
+            map2(pl, pr)((a, b) => a.concat(b))
+        }
+    }
+
+
+    // có thể dùng simpleSequence hoặc là balanceSequence
+    def sequence[A](ps: List[Par[A]]): Par[List[A]] = {
+        /*simpleSequence(ps)*/
+        map(balanceSequence(ps.toIndexedSeq))(idxSeq => idxSeq.toList)
+    }
+
+    def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = {
+        folk({
+            val lpb = ps.map(a => {
+                val ff = asyncF(f)
+                ff(a)
+            })
+            sequence(lpb)
+        })
     }
 
 
@@ -122,12 +175,27 @@ object Par {
         folk[A](unit[A](a))
     }
 
+    def map[A, B](parA: Par[A])(fMap: A => B): Par[B] = {
+        map2(parA, unit(()))((a, _) => fMap(a))
+    }
+
+
+    /*def flatMap[A, B](par: Par[A])(f: A => Par[B]): Par[B] = {
+
+    }*/
+
 
     // sử dụng lazyUnit để viết hàm này
-    // async 1 hàm
+    // async 1 hàm bất kỳ (từ 1 hàm A=>B sẽ trả ra 1 hàm A=> Par[B])
+    // nó sẽ chạy hàm map trên một thread khác
     def asyncF[A, B](f: A => B): A => Par[B] = {
         a: A => lazyUnit(f(a))
     }
+
+    def sortList(source: Par[List[Int]]): Par[List[Int]] = {
+        map(source)(_.sorted)
+    }
+
 
 }
 

383ce4ca891bfa187bc8be48ee68ae4143b5b40f
magicghostvu <magicghost.vu@gmail.com>
2020-04-06 20:29:46 +0700

    #replace mutable map with immutable map

--- src/main/scala/learn_fp_in_scala/chap7/FilterTextInFolder.scala
+++ src/main/scala/learn_fp_in_scala/chap7/FilterTextInFolder.scala
@@ -5,18 +5,22 @@ import java.util
 import java.util.concurrent.{ExecutorService, Executors}
 import java.util.stream.Collectors
 
+import com.google.gson.Gson
 import learn_fp_in_scala.chap7.Par._
+import org.apache.commons.io.FileUtils
 import useoop.MLogger
 
+import scala.collection.immutable.HashMap
+import scala.collection.mutable
 import scala.jdk.javaapi.CollectionConverters
 
 
 object FilterTextInFolder {
     implicit val executor: ExecutorService = Executors.newFixedThreadPool(10)
 
-    private val uid = "Payment Update Param: "
+    private val uid = ""
 
-    private val ACTION = ""
+    private val ACTION = "JACK_POT_INGAME"
 
     private val p1 = ""
 
@@ -29,12 +33,24 @@ object FilterTextInFolder {
     }
 
 
+    case class UserWinJackpot(uid: Int, goldWin: Long, avatarUrl: String, displayName: String)
+
+
+    // hợp 2 user lại thành 1
+    def combine2User(u1: UserWinJackpot, u2: UserWinJackpot): UserWinJackpot = {
+        val newGold = u1.goldWin + u2.goldWin
+        u1.copy(goldWin = newGold)
+    }
+
+
     def main(args: Array[String]): Unit = {
-        val pathToFolder = "C:\\Users\\phuvh\\Desktop\\payment_log_04_01"
+
+
+        val pathToFolder = "E:\\work_at_home\\log_process"
         val folderAllFile = new File(pathToFolder)
         val allChildren = folderAllFile.listFiles()
         val fListString = Par.parMap(allChildren.toList)(file => {
-            MLogger.generalLogger.debug("file is {}", file.getName)
+            //MLogger.generalLogger.debug("file is {}", file.getName)
             val fr = new FileReader(file)
             val bReader = new BufferedReader(fr)
             val stream = bReader.lines()
@@ -50,13 +66,48 @@ object FilterTextInFolder {
         val res = listFinal.run().get()
 
 
-
-
-
         val end = System.currentTimeMillis()
         MLogger.generalLogger.debug("time is {}, size is {}", end - start, res.size)
 
-        res.foreach(println(_))
+        //
+        //res.foreach(println(_))
+        val listFinalUser = res
+            .map(str => {
+                val tmp = str.split("\\|\\|")
+                val uid = tmp(1).split(";")(1).toInt
+                val arrNameGoldUrl = tmp(2).split(";")
+                val goldWin = arrNameGoldUrl(1).toLong
+                val name = arrNameGoldUrl(0)
+                val avatarUrl = arrNameGoldUrl(2)
+                UserWinJackpot(uid, goldWin, avatarUrl, name)
+            })
+            .foldRight(HashMap[Int, UserWinJackpot]())((u, m) => {
+                /*if (m.contains(u.uid)) {
+                    val old = m.get(u.uid).head
+                    val newUser = combine2User(old, u)
+                    m.put(u.uid, newUser)
+                } else {
+                    m.put(u.uid, u)
+                }
+                m*/
+                if (m.contains(u.uid)) {
+                    val old = m.get(u.uid).head
+                    val newUser = combine2User(old, u)
+                    m.+((u.uid, newUser))
+
+                } else {
+                    m.+((u.uid, u))
+                    //m2
+                }
+            })
+            .values.toList
+        val listJava = CollectionConverters.asJava(listFinalUser)
+        val gson = new Gson()
+        val strToFile = gson.toJson(listJava)
+        val path = System.getProperty("user.dir") + "/fileUser.json"
+        FileUtils.writeStringToFile(new File(path), strToFile, "utf-8", false)
+        println("done")
+
 
     }
 }

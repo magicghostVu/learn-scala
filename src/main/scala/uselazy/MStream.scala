2693e939e0a42fd2fe108578771437d8a2a39e12
magicghostvu <magicghost.vu@gmail.com>
2020-07-13 17:52:30 +0700

    #thêm head option cài đặt bằng foldRight

--- src/main/scala/uselazy/MStream.scala
+++ src/main/scala/uselazy/MStream.scala
@@ -11,6 +11,17 @@ sealed trait MStream[+A] {
         }
     }
 
+
+    def headOption2(): Option[A] = {
+        val o: Option[A] = None
+        foldRight(o)((a, b) => {
+
+            println(s"run fold a is $a")
+
+            Some(a)
+        })
+    }
+
     @tailrec
     final def exist(condition: A => Boolean): Boolean = {
         this match {
@@ -27,18 +38,27 @@ sealed trait MStream[+A] {
 
 
     // hàm này ngu ở chỗ nó phải duyệt hết cả stream,
-    // tuy nhiên diểm sáng là nó sẽ không tính toán lại các biểu thức cond(a) sau khi đã gặp giá trị true
+    // tuy nhiên diểm sáng là nó sẽ không tính toán lại các biểu thức cond(a) về sau khi đã gặp giá trị true
     def exist2(cond: A => Boolean): Boolean = {
-        foldRight[Boolean](false)((a, b) => cond(a) || b)
+        foldRight[Boolean](false)((currentElement, currentBool) => currentBool || cond(currentElement))
     }
 
     // sẽ trả về giá trị tích lũy ngay khi gặp empty
     // hàm fold này chưa được tối ưu do sử dụng đệ quy đầu
-    def foldRight[B](currentAcc: => B)(f: (A, => B) => B): B = {
+    def foldRight[B](currentAcc: => B)(foldFunction: (A, => B) => B): B = {
         this match {
-            case Empty => currentAcc
+            case Empty => { // nếu stream đã hết thì trả về giá trị tích lũy hiện tại
+
+                println("đã hểt")
+
+
+                currentAcc
+            }
             case Cons(head, tail) => {
-                f(head(), tail().foldRight(currentAcc)(f))
+
+                println("run fold h is " + head())
+
+                foldFunction(head(), tail().foldRight(currentAcc)(foldFunction))
             }
         }
     }
@@ -47,6 +67,18 @@ sealed trait MStream[+A] {
     def forAll(condition: A => Boolean): Boolean = {
         foldRight[Boolean](false)((a, b) => condition(a) && b)
     }
+
+
+    def forEach(function: A => Unit): Unit = {
+        this match {
+            case Cons(head, tail) => {
+                function(head())
+                tail().forEach(function)
+            }
+            case _ =>
+        }
+    }
+
 
 }
 
@@ -60,25 +92,34 @@ object MStream {
 
     // sử dụng call by name để delay evaluation
     def cons[A](head: => A, tail: => MStream[A]): MStream[A] = {
+
+        // tạo mới các biến lazy để delay evaluation các tham số bên trên trong trường hợp các tham số này là một hàm
         lazy val h = head
         lazy val t = tail
+
+        //tạo mới các function để delay việc tính toán giá trị của head và tail
+        //giá trị bên trong head và tail chỉ thực sự được tính toán khi gọi đến
         Cons(() => h, () => t)
     }
 
 
-    def apply[A](a: A*): MStream[A] = {
-        if (a.isEmpty) {
+    def apply[A](elements: A*): MStream[A] = {
+        if (elements.isEmpty) {
             empty
         } else {
-            cons(a.head, apply(a.tail: _*))
+            //val h = elements.head
+            //println(s"h is $h")
+            cons(elements.head, apply(elements.tail: _*))
         }
     }
+
 
     @tailrec
     private def toList[A](current: List[A], stream: MStream[A]): List[A] = {
         stream match {
             case Empty => current
             case Cons(head, tail) => {
+                // nối giá trị hiện tại vào đầu danh sách tích lũy
                 toList(head() :: current, tail())
             }
         }
@@ -89,13 +130,14 @@ object MStream {
     }
 
     def toListFast[A](stream: MStream[A]): List[A] = {
-
         @tailrec
-        def funRecursive[B](buff: ListBuffer[B], stream: MStream[B]): ListBuffer[B] = {
-            stream match {
-                case Empty => buff
+        def funRecursive[B](buffAccumulate: ListBuffer[B], streamRemain: MStream[B]): ListBuffer[B] = {
+            streamRemain match {
+                case Empty => buffAccumulate
                 case Cons(head, tail) => {
-                    funRecursive(buff.appended(head()), tail())
+                    val h = head()
+                    println(s"head is $h")
+                    funRecursive(buffAccumulate.appended(head()), tail())
                 }
             }
         }
@@ -109,14 +151,16 @@ object MStream {
         if (n <= 0) empty
         else {
             @tailrec
-            def takeRecursive[A](count: Int, target: Int,
-                                 streamCr: MStream[A], streamAcc: MStream[A]): MStream[A] = {
+            def takeRecursive[X](count: Int, target: Int,
+                                 streamCr: MStream[X], streamAcc: MStream[X]): MStream[X] = {
                 streamCr match {
                     // trong trường hợp n vượt quá size của origin stream
                     case Empty => streamAcc
                     case Cons(head, tail) => {
                         if (count == target) streamAcc
                         else {
+
+                            // nối vào đầu thì chả ngược
                             takeRecursive(count + 1, target, tail(), cons(head(), streamAcc))
                         }
                     }
@@ -203,13 +247,14 @@ object MStream {
     }
 
     def takeWhile2[A](condition: A => Boolean, stream: MStream[A]): MStream[A] = {
-        stream.foldRight[MStream[A]](empty)((currentVal, currentAcc) => {
+        stream.foldRight[MStream[A]](empty)((currentVal, currentStreamAcc) => {
 
-            // nếu
+            // nếu vẫn còn phần tử thỏa mãn thì nối giá trị hiện tại vào
             if (condition(currentVal)) {
-                //println(s"a is $a")
-                cons(currentVal, currentAcc)
-            } else {
+                println(s"a is $currentVal")
+                cons(currentVal, currentStreamAcc)
+            }
+            else {
                 empty
             }
         })
--- src/main/scala/uselazy/MainLazy.scala
+++ src/main/scala/uselazy/MainLazy.scala
@@ -8,9 +8,33 @@ object MainLazy {
     }
 
     def main(args: Array[String]): Unit = {
-        val mStream = MStream(1, 3, 4, 56, 68, 78, 1)
-        val p = mStream.toListFast()
-        println(mStream.takeWhile(a => a <= 3).toListFast())
+
+
+        val mList = List(3, 6, 7, 89, 9, 4, 3)
+
+        val ii = MStream(2, 5, 6, 7, 9)
+
+        /*
+        val mStream = MStream(1, 3, 4, 56, 68, 78)
+
+        mStream.forEach(i => {
+            println(s"i is $i")
+        })*/
+
+
+        val p = ii.headOption2
+
+        println(s"p is $p")
+
+
+        //println()
+
+        /*val io = mStream.headOption.get
+
+        println(s"io is $io")*/
+
+        /*val p = mStream.toListFast()
+        println(mStream.takeWhile(a => a <= 3).toListFast())*/
 
     }
 }

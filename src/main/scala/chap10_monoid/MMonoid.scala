9e7079a91ed7e12c59bf3c9d9c5bfc7ba5fc687b
magicghostvu <magicghost.vu@gmail.com>
2020-10-14 17:45:53 +0700

    #add some example chap 10

--- src/main/scala/chap10_monoid/MMonoid.scala
+++ src/main/scala/chap10_monoid/MMonoid.scala
@@ -32,6 +32,43 @@ object MMonoid {
     }
 
 
+    val stringMonoid: MMonoid[String] = new MMonoid[String] {
+        override def op(a1: String, a2: String): String = a1 + a2
+
+        override val zero: String = ""
+    }
+
+
+    def concatList[A](la: List[A])(monoid: MMonoid[A]): A = {
+        la.foldRight(monoid.zero)(monoid.op)
+    }
+
+
+    def endoMonoid[A]: MMonoid[A => A] = new MMonoid[A => A] {
+        override def op(a1: A => A, a2: A => A): A => A = a1.andThen(a2)
+
+        override val zero: A => A = (a: A) => a
+    }
+
+
+    def foldMap[A, B](listA: List[A], monoIdB: MMonoid[B])(f: A => B): B = {
+        concatList(listA.map(f))(monoIdB)
+    }
+
+
+
+    // ý tưởng chính là biến List[A] và monoid[B] và cái f thành một function dạng B=> B, sau đó áp function này với startVal
+    def foldRight[A, B](la: List[A], monoid: MMonoid[B])(startVal:B)(f: (A,B) => B) = {
+        val fCurried= f.curried
+
+
+        //    foldMap()
+
+        val newF= foldMap()
+
+    }
+
+
     //tuy là monoid này vẫn thỏa mãn monoid law nhưng mà nó sẽ
     // không có tính chất giao hoán với đối với 2 tham số trong op
     def optionMonoid[A]: MMonoid[Option[A]] = new MMonoid[Option[A]] {
--- src/main/scala/chap10_monoid/RunChap10.scala
+++ src/main/scala/chap10_monoid/RunChap10.scala
@@ -1,8 +1,27 @@
 package chap10_monoid
 
+import chap10_monoid.MMonoid.{foldMap, intAddition}
+
 object RunChap10 {
     def main(args: Array[String]): Unit = {
+        val lString = List("phuvh", "haonc", "khangvt")
+        val acc = foldMap(lString, intAddition)(str => str.length)
+        println(s"acc is $acc")
+
+
+        val ff: (Int, Double) => Double = (a, b) => {
+            a + b
+        }
+
+
+        val ff2 = ff.curried
+
+        val f3 = ff2(5)
+
+        val b = f3(6)
+
 
     }
+
 
 }
